d564a827c183e74f4e2933a3afbad884bf32761f
jurgen69420 <99071244@mydavinci.nl>
2024-02-06 10:17:05 +0100

    poging 1 - demo code

---
+++ poging 1.py
@@ -0,0 +1,19 @@
+class Pokemon:
+    def __init__(self, name, level, type):
+        self.name = name
+        self.level = level
+        self.type = type
+
+    def attack(self):
+        print(f"{self.name} attacks with a powerful move!")
+
+# Create two Pokemon instances
+pokemon1 = Pokemon("Pikachu", 10, "Electric")
+pokemon2 = Pokemon("Charmander", 15, "Fire")
+
+# Simulate a battle
+print(f"A wild {pokemon2.name} appeared!")
+print(f"Go, {pokemon1.name}!")
+
+pokemon1.attack()
+pokemon2.attack()
