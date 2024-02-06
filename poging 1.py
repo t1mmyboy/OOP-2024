477b72feef412b0790d97f556c94dff68355241b
jurgen69420 <99071244@mydavinci.nl>
2024-02-06 11:35:13 +0100

    klaar denk ik

--- poging 1.py
+++ poging 1.py
@@ -1,21 +1,37 @@
-class Pokemon:
-    def __init__(self, name, level, type):
-        self.name = name
-        self.level = level
-        self.type = type
-
-    def attack(self):
-        print(f"{self.name} attacks with a powerful move!")
-
-# Create two Pokemon instances
-pokemon1 = Pokemon("Pikachu", 10, "Electric")
-pokemon2 = Pokemon("Charmander", 15, "Fire")
-
-# Simulate a battle
-print(f"A wild {pokemon2.name} appeared!")
-print(f"Go, {pokemon1.name}!")
-
-print(f"gemaakt in de Jorgon branch")
-
-pokemon1.attack()
-pokemon2.attack()
+repeat = True
+#The player start the game.
+print("Welcome tot de wereld van pokemon!")
+print("wilt u starten? (ja/nee)")
+if input() == "ja":
+    print("je bent nu in de wereld van pokemon!")
+else:
+    print("bye!")
+    exit()
+#The player gives a name to a charmander.
+print("geef je charmander een naam")
+name = input()
+print("je charmander heet nu " + name)
+#The charmander does its battle cry for ten times.
+for i in range(10):
+    print(name + " doet zijn battle cry!")
+#The player can give a new name to the same charmander.
+while repeat == True:
+    print("wil je " + name + " een nieuwe naam geven? (ja/nee)")
+    if input() == "ja":
+        print("geef je charmander een nieuwe naam")
+        name = input()
+        print("je charmander heet nu " + name)
+        for i in range(10):
+         print(name + " doet zijn battle cry!")
+    else:
+        print("ok")
+#The charmander does its battle cry for ten times.
+        for i in range(10):
+         print(name + " doet zijn battle cry!")
+#Repeat 4 and 5 until the player quits the game.
+    print("wil je stoppen? (ja/nee)")
+    if input() == "ja":
+        print("bye!")
+        exit()
+    else:
+        Repeat = True
