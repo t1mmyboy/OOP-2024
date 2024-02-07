e0302f7206b70190efd54fe4041dffbad151909e
jurgen69420 <99071244@mydavinci.nl>
2024-02-07 13:37:10 +0100

    pokeb

--- pokeball.py
+++ pokeball.py
@@ -4,8 +4,7 @@ class pokeball:
         self.pokemon = pokemon
         self.isempty = isempty
 pokeball1 = pokeball(charmander,False)
-pokeball2 = pokeball(,
-                    False)
+pokeball2 = pokeball(False)
 pokeball3 = pokeball(False)
 pokeball4 = pokeball(False)
 pokeball5 = pokeball(False)
