0e9e32b6d15540ed14cfcb4c99ee88c00627c03a
jurgen69420 <99071244@mydavinci.nl>
2024-02-07 13:36:30 +0100

    pokeball

---
+++ pokeball.py
@@ -0,0 +1,12 @@
+from pokemon import *
+class pokeball:
+    def __init__(self,pokemon,isempty):
+        self.pokemon = pokemon
+        self.isempty = isempty
+pokeball1 = pokeball(charmander,False)
+pokeball2 = pokeball(,
+                    False)
+pokeball3 = pokeball(False)
+pokeball4 = pokeball(False)
+pokeball5 = pokeball(False)
+pokeball6 = pokeball(False)
