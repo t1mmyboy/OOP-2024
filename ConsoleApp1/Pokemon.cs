7b0929a9fbe79020d442cb5bc2d2adc9b46d6a03
t1mmyboy <99074833@mydavinci.nl>
2024-02-08 11:52:55 +0100

    test2

---
+++ ConsoleApp1/Pokemon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Pokemon {
+    public static void Main (string[] args)
+    {
+        Pokemon pikachu = new Pokemon("Pikachu", "Sparky", "Electric", "Ground");
+    }
+}
+
+class Pokemon
+{
+    public string species;
+    public string name;
+    public string type;
+    public string weakness;
+
+    public Pokemon(string species, string name, string type, string weakness)
+    {
+        this.species = species;
+        this.name = name;
+        this.type = type;
+        this.weakness = weakness;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+    public string getType()
+    {
+        return type;
+    }
+    public string getSpecies()
+    {
+        return species;
+    }
+    public string getWeakness()
+    {
+        return weakness;
+    }
+    
+}
